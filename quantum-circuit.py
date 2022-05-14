cc0825f6583b396c852d0b0f456cc15d7e2cb7e7
LegacYFTw <turbasu.chatterjee@gmail.com>
2022-05-14 10:17:18 +0530

    Added support for classical register.

--- quantum-circuit.py
+++ quantum-circuit.py
@@ -26,18 +26,19 @@ import typing
 
 class QuantumCircuit:
     """"
-    x = QuantumCircuit((3,3))
+    x = QuantumCircuit((3,3),1)
     This will basically create a QuantumCircuit with RegisterLength = 3, meaning there shall be 3 qudits, with energy
-    level 3 or in essence a 3 qutrit register.
+    level 3 or in essence a 3 qutrit register and one classical register.
 
-    y = QuantumCircuit([2,2,3,3])
+    y = QuantumCircuit([2,2,3,3], 1)
     This will create a QuantumCircuit with RegisterLength = len(Iterable) (here 4) with the energy levels 2,2,3,3 or in
-    other words this will create a two qubits and two qutrits.
+    other words this will create a two qubits and two qutrits and one classical register.
     """
 
 
     def __init__(self,
                  qregs: Union[Tuple[int, int], List[int]],
+                 cregs: int = 1,
                  name: Optional[str] = None
                  ):
         if any(not isinstance(reg, (Tuple, List)) for reg in qregs):
@@ -56,6 +57,8 @@ class QuantumCircuit:
                 size_of_register = len(qregs)
 
             return size_of_register
+
+
 
 
 
