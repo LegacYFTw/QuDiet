9705c2b1d70db985eca6b1320032fcb95c65ba1c
LegacYFTw <turbasu.chatterjee@gmail.com>
2022-07-03 12:06:15 +0530

    Pushed measurement.py with completed Measurement gate.

--- circuit_library/standard_gates/measurement.py
+++ circuit_library/standard_gates/measurement.py
@@ -1,7 +1,45 @@
-from scipy import sparse
-from scipy.sparse import csr_matrix, coo_matrix, csc_matrix
-import numpy as np
+from abc import ABC
+from typing import Union
+
+from circuit_library.standard_gates.quantum_gate import QuantumGate
 
 
-class Measurement:
-    pass
+class Measurement(QuantumGate, ABC):
+    def __init__(self, qreg: 'Union[tuple[int, int], list[int]]'):
+        super().__init__(qreg)
+        self._qreg = qreg
+
+    @property
+    def is_controlled(self) -> bool:
+        """
+        Check if the gate is controlled or not
+        :return: True or False, depending on the scenario
+        """
+        return False
+
+    @property
+    def is_single_qudit(self) -> bool:
+        """
+        Check if the gate is a single qudit or multi-qudit
+        :return: True or False, depending on the scenario
+        """
+        return True
+
+    @property
+    def unitary(self) -> None:
+        """
+        This is the gate unitary which shall be used to do any calculations
+        :return: The gate unitary
+        """
+        return None
+
+    @property
+    def acting_on(self) -> Union[int, list]:
+        """
+        Gets the index of the acting qudit in the QuantumRegister
+        :return: Index of the QuantumRegister if it is a single qudit gate or a list if multiqudit
+        """
+        return self._qreg
+
+
+
