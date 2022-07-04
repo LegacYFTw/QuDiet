892eb02aa1aa263bf44fbc6cc8990a2c6977952f
LegacYFTw <turbasu.chatterjee@gmail.com>
2022-07-04 15:55:12 +0530

    Completed the CXGate. Added qasm and tests folders for further functionality.

--- circuit_library/standard_gates/cx.py
+++ circuit_library/standard_gates/cx.py
@@ -1,7 +1,10 @@
 from abc import ABC
 from typing import Union
 
+import numpy as np
 from scipy import sparse
+from scipy.linalg import circulant
+from scipy.sparse import csr_matrix
 
 from circuit_library.standard_gates.quantum_gate import QuantumGate
 
@@ -12,11 +15,13 @@ class CXGate(QuantumGate, ABC):
                  dims: int
                  ):
         """
-        :param qreg:
-        :param dims:
-
-
+        This generates the CX Gate object for a given set of dimension and a qreg tuple representing control and target.
+        :param qreg: (control, target). This represents the control and the target qudits on which the CXGate shall act
+                     on. This is a two qudit quantum gate.
+        :param dims: This represents the dimensions of the CXGate
         """
+        self._qreg = qreg
+        self._dims = dims
 
     @property
     def is_controlled(self) -> bool:
@@ -40,7 +45,26 @@ class CXGate(QuantumGate, ABC):
         This is the gate unitary which shall be used to do any calculations
         :return: The gate unitary
         """
-        return None
+
+        _x_unitary_builder = np.zeros(shape=(self._dims, 1))
+        _x_unitary_builder[1] = 1
+        _x_unitary = csr_matrix(circulant(_x_unitary_builder))
+
+        _O = np.zeros((self._dims, self._dims))
+        _I = np.eye(self._dims)
+
+        for _ in range(self._dims):
+            _hstack_list = []
+            for __ in range(self._dims):
+                _hstack_p = [_O for _ in range(__)]
+                _hstack_m = _x_unitary if _ == __ == (self._dims - 1) else _I
+                _hstack_s = [_O for _ in range(self._dims - __ - 1)]
+
+                _hstack_list += [np.hstack((*_hstack_p, _hstack_m, *_hstack_s))]
+
+            _vstack_list = np.vstack(_hstack_list)
+
+        return csr_matrix(_vstack_list)
 
     @property
     def acting_on(self) -> Union[int, list]:
