52e72ecc092961612f1f4988502c92c66dee6a00
Turbasu Chatterjee <turbasu.chatterjee@gmail.com>
2022-07-24 16:52:18 +0000

    bugs

--- testbench/tof_qutrit_py/4mod5-bdd_287_tof.py
+++ testbench/tof_qutrit_py/4mod5-bdd_287_tof.py
@@ -1,31 +1,31 @@
 ## Time taken = 0.18038925994187593
 
-import timeit
-import_module = '''
+import time
 from framework.core.quantum_circuit import QuantumCircuit
-'''
-testcode = ''' 
+
 def test(): 
     qc = QuantumCircuit(qregs=[2, 2, 2, 2, 3, 3, 3])
 
-    qc.cx(3, 4)
-    qc.cx(1, 4)
-    qc.cx(0, 4)
-    qc.cx(4, 5)
-    qc.cx(0, 4)
-    qc.cx(0, 4)
-    qc.cx(4, 6)
-    qc.cx(0, 4)
-    qc.cx(4, 6)
+    qc.cx((3, 4), plus=1)
+    qc.cx((1, 4), plus=1)
+    qc.cx((0, 4), plus=1)
+    qc.cx((4, 5), plus=1)
+    qc.cx((0, 4), plus=1)
+    qc.cx((0, 4), plus=1)
+    qc.cx((4, 6), plus=1)
+    qc.cx((0, 4), plus=1)
+    qc.cx((4, 6), plus=1)
     qc.x(6)
-    qc.cx(2, 6)
-    qc.cx(6, 5)
-    qc.cx(2, 6)
-    qc.cx(2, 5)
-    qc.cx(5, 6)
-    qc.cx(2, 5)
+    qc.cx((2, 6), plus=1)
+    qc.cx((6, 5), plus=1)
+    qc.cx((2, 6), plus=1)
+    qc.cx((2, 5), plus=1)
+    qc.cx((5, 6), plus=1)
+    qc.cx((2, 5), plus=1)
+    qc.measure_all()
+    qc.run
 
-'''
-
-
-print(timeit.timeit(stmt=testcode, setup=import_module))
+start = time.time()
+test()
+end = time.time()
+print(start - end)
