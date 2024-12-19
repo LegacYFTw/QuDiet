b2ef06aa4b0b668759ba1c236b729c1851bf3823
Arnav Das <arnav.das88@gmail.com>
2024-12-19 14:18:57 +0000

    Updated measurement with shots count
    Signed-off-by: Arnav Das <arnav.das88@gmail.com>

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ from qudiet.core.quantum_circuit import QuantumCircuit
 from qudiet.core.backend.SparseBackend import SparseBackend
 from qudiet.core.backend.NumpyBackend import NumpyBackend
 
+from collections import Counter
+
 start_time = time.time()
 
 # backend = SparseBackend
 backend = NumpyBackend
 
-qc = QuantumCircuit(
-    qregs=[2, 2, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2],
-    init_states=[1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    backend=backend
-)
+# qc = QuantumCircuit(
+#     qregs=[2, 2, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2],
+#     init_states=[1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+#     backend=backend
+# )
 
 # qc.cx(acting_on=(0, 2), plus=1)
 # qc.cx(acting_on=(2, 5), plus=1)
@@ -37,10 +39,26 @@ qc = QuantumCircuit(
 # qc.cx(acting_on=(8, 12), plus=1)
 # qc.cx(acting_on=(9, 12), plus=1)
 
+
+qc = QuantumCircuit(
+    qregs=[2, ],
+    init_states=[0, ],
+    backend=backend,
+    shots=1
+)
+qc.h(0)
+
 qc.measure_all()
 
 result = qc.run()
 
-
 end_time = time.time()
 print(f"Time elapsed: {end_time - start_time}s")
+# print(f"Result: {result}")
+
+result = Counter(result)
+print(f"Result: {result}")
+
+
+
+
